57e221530b5063ef1ce81b748febe90cf99d012b
Roberto Cornice <roberto@punktquadrat.de>
2026-05-02 23:23:06 +0200

    ui: use Surface and MaterialTheme colors in SettingsScreen

--- app/src/main/java/de/robnice/homeshoplist/ui/screens/SettingsScreen.kt
+++ app/src/main/java/de/robnice/homeshoplist/ui/screens/SettingsScreen.kt
@@ -38,7 +38,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -318,10 +317,10 @@ fun SettingsScreen(
         }
     }
 
-    CompositionLocalProvider(
-        LocalTextStyle provides LocalTextStyle.current.copy(
-            color = Color.Black
-        )
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background,
+        contentColor = MaterialTheme.colorScheme.onBackground
     ) {
         Column(
             modifier = Modifier
