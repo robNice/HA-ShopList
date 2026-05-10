0134d0258adf9dbf534e132578fb8b7deae881c8
Roberto Cornice <roberto@punktquadrat.de>
2026-05-11 00:48:43 +0200

    build: bump version to 1.9.2.0
    - Update `versionName` in `app/build.gradle.kts` from 1.9.1.7 to 1.9.2.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 901
-        versionName = "1.9.1.7"
+        versionName = "1.9.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
